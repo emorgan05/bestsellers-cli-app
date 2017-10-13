8d058c341b7e5101b540913a4841fe983471524c
Emily Morgan <emorgan05@gmail.com>
2017-10-13 14:47:13 +0000

    list_books method in CLI

--- lib/bestsellers/cli.rb
+++ lib/bestsellers/cli.rb
@@ -4,13 +4,23 @@ class Bestsellers::CLI
     puts "Welcome to the New York Times Bestseller List!"
     puts "Are you ready to find your next good read?"
     puts ""
+    list_section
   end
 
   def list_section
     puts "1. Print and Ebook Fiction"
     puts "2. Hardcover Fiction"
     puts "3. Print and Ebook Nonfiction"
-    puts "Type the "
+    puts "Choose a number to see the top five books in that section:"
+    section_input = gets.strip.to_i
+  end
+
+  def list_books
+    puts "1. Origin by Dan Brown"
+    puts "2. The Sun and Her Flowers by Rupi Kaur"
+    puts "3. Sleeping Beauties by Steven King and Owen King"
+    puts "Choose a number for full details about the book, or type 'category list' to return to the list of categories, or 'exit'"
+    books_input = gets.strip
   end
 
 end
