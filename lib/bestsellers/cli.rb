89dad6dabaef3ad8a0fc17295ddf239b4345fe9f
Emily Morgan <emorgan05@gmail.com>
2017-10-13 14:42:38 +0000

    Call and list_section methods in CLI

---
+++ lib/bestsellers/cli.rb
@@ -0,0 +1,16 @@
+class Bestsellers::CLI
+
+  def call
+    puts "Welcome to the New York Times Bestseller List!"
+    puts "Are you ready to find your next good read?"
+    puts ""
+  end
+
+  def list_section
+    puts "1. Print and Ebook Fiction"
+    puts "2. Hardcover Fiction"
+    puts "3. Print and Ebook Nonfiction"
+    puts "Type the "
+  end
+
+end
