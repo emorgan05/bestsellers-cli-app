043c511af38650db7fd16e46212c0bb660cf6fa7
Emily Morgan <emorgan05@gmail.com>
2017-10-13 18:35:14 +0000

    Recommitting from the proper file. Added and required classes.

--- lib/bestsellers.rb
+++ lib/bestsellers.rb
@@ -1,5 +1,8 @@
 require "bestsellers/version"
 require "bestsellers/cli"
+require "bestsellers/scraper"
+require "bestsellers/book"
+require "bestsellers/category"
 
 module Bestsellers
   # Your code goes here...
--- lib/bestsellers/scraper.rb
+++ lib/bestsellers/scraper.rb
@@ -3,5 +3,7 @@ require "open-uri"
 require "pry"
 
 class Scraper
+  doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
 
+  binding.pry
 end
