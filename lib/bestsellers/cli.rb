0a0eeb427ea37c87edecfd748f118a909bc827d6
Emily Morgan <emorgan05@gmail.com>
2017-10-18 21:02:29 +0000

    Refactoring the CLI

--- lib/bestsellers/cli.rb
+++ lib/bestsellers/cli.rb
@@ -5,14 +5,36 @@ class Bestsellers::CLI
     puts "Welcome to the New York Times Bestseller List!"
     puts "Are you ready to find your next good read?"
     puts ""
+    menu
+  end
 
+  def menu
+    input = nil
     list_categories
-    puts ""
-    puts "Choose a number to see the top five books in that category:"
-    @category_input = gets.strip
-    
-    list_books
-    book_details
+    category_choice
+    if @category_input == "exit"
+      goodbye
+    else
+      @category_input = @category_input.to_i
+      list_books
+      books_choice
+      if @books_input == "exit"
+        goodbye
+      elsif @books_input == "category list"
+        menu
+      else
+        @books_input = @books_input.to_i
+        book_details
+        details_choice
+        if @details_input == "exit"
+          goodbye
+        elsif @details_input == "category list"
+          menu
+        elsif @details_input == "book_list"
+          menu
+        end
+      end
+    end
   end
 
   def list_categories
@@ -21,47 +43,41 @@ class Bestsellers::CLI
     end
   end
 
+  def category_choice
+    puts "Choose a number to see the top five books in that category or type 'exit'"
+    @category_input = gets.strip
+  end
+
   def list_books
-    category = Bestsellers::Category.all[@category_input.to_i - 1].name
+    category = Bestsellers::Category.all[@category_input - 1].name
 
     @book_array = Bestsellers::Book.find_by_category(category)
 
     @book_array.each.with_index(1) do |book, index|
       puts "#{index}. #{book.title} by #{book.author}"
     end
+  end
 
-    puts ""
+  def books_choice
     puts "Choose a number for full details about the book, type 'category list' to return to the list of categories, or 'exit'"
     @books_input = gets.strip
-
-    case @books_input
-    when is_a?(Integer) # problem here
-      book_details
-    when 'category list'
-      list_categories
-    when 'exit'
-      exit
-    end
   end
 
   def book_details
-    book = @book_array[@books_input.to_i - 1]
+    book = @book_array[@books_input - 1]
     puts "#{book.title} by #{book.author}"
     puts "Summary: #{book.description}"
     puts "Weeks on the Bestsellers List: #{book.weeks_on_list}"
     puts "Find on Amazon: #{book.amazon_url}"
     puts ""
-
-    puts "Type 'book list' to return to the list of books in this section, type 'category list' to return to the list of categories, or 'exit'"
-    @book_details_input = gets.strip
-    case @book_details_input
-    when 'book list'
-      list_books
-    when 'category list'
-      list_categories
-    when 'exit'
-      exit
-    end
   end
 
+  def details_choice
+    puts "Type 'book list' to return to the list of books in this section, type 'category list' to return to the list of categories, or 'exit'"
+    @details_input = gets.strip
+  end
+
+  def goodbye
+    "Come back soon for your next good read!"
+  end
 end
