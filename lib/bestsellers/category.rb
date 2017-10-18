8ab585adb810a5f00bd11f7fabe26de8210a45c6
Emily Morgan <emorgan05@gmail.com>
2017-10-18 18:52:42 +0000

    Developed Bestsellers::Category class

--- lib/bestsellers/category.rb
+++ lib/bestsellers/category.rb
@@ -1,3 +1,24 @@
 class Bestsellers::Category
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    if !@@all.include?(self)
+      @@all << self
+    end
+  end
+
+  def self.new_from_json(list)
+    self.new(list["list_name"]).save
+  end
+
+  def self.all
+    @@all
+  end
 
 end
