986c5b78039bb8d64ce7feeeb51b381645a65029
Emily Morgan <emorgan05@gmail.com>
2017-10-18 18:30:03 +0000

    Got the API call working

--- lib/bestsellers/api.rb
+++ lib/bestsellers/api.rb
@@ -1,12 +1,14 @@
 class Bestsellers::API
-  attr_accessor
+  attr_accessor :list_data, :lists
 
   def self.bestsellers_list
-    lists = RestClient.get("https://api.nytimes.com/svc/books/v3/lists/overview.json")
+    lists = RestClient.get("https://api.nytimes.com/svc/books/v3/lists/overview.json", params: { "api-key" => "c95058c8a60a42c5b9c063e4901717a2" })
     @list_data = JSON.parse(lists)
-    binding.pry
-    @list_data["results"].each do |books|
-          Bestsellers::Book.new_from_json(books)
+    @list_data["results"]["lists"].each do |list|
+      Bestsellers::Category.new_from_json(list)
+      list["books"].each do |book|
+        Bestsellers::Books.new_from_json(book)
+      end
     end
   end
 
